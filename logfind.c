b3f6f0b893b4d0def48819311b78e67bf7072f89
rufevean <deerajmydearalways@gmail.com>
2024-06-01 00:16:32 +0530

    added errors, found the files

--- logfind.c
+++ logfind.c
@@ -1,47 +1,89 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-char *ltrim(char *s);
+#include <errno.h>
+
 #define MAX_LINES 100
 #define MAX_LINE_LENGTH 256
 
-int main(int argc, char** argv[]){
-    FILE *file;
-    file  = fopen("./.flogs", "r");
-    
-    if (file == NULL){
-        printf("Error: Could not open file\n");
+char *rtrim(char *s) {
+    char *back = s + strlen(s);
+    while (isspace((unsigned char)*(--back)));
+    *(back + 1) = '\0';
+    return s;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Usage: %s [-o , -a] pattern [pattern ...]\n", argv[0]);
         return 1;
     }
-    char lines[MAX_LINES];
+
+    int or_flag = 0, and_flag = 0;
+    if (strcmp(argv[1], "-o") == 0) {
+        or_flag = 1;
+    } else if (strcmp(argv[1], "-a") == 0) {
+        and_flag = 1;
+    } else {
+        printf("Error: You must specify either -o (OR) or -a (AND) as the first argument.\n");
+        return 1;
+    }
+
+    FILE *file = fopen("./.flogs", "r");
+    if (file == NULL) {
+        perror("Error opening .flogs file");
+        return 1;
+    }
+
+    char lines[MAX_LINE_LENGTH];
     char files[MAX_LINES][MAX_LINE_LENGTH];
-    int i = 0 ;
-    while(fgets(lines, MAX_LINES, file) != NULL){
+    int i = 0;
+    while (fgets(lines, sizeof(lines), file) != NULL && i < MAX_LINES) {
+        rtrim(lines);
         strcpy(files[i], lines);
         i++;
     }
-    
-    for (int j = 0; j < i; j++){
-        printf("File %d: %s", j, files[j]);
-        FILE *newfile = fopen( files[j], "r");
-        if (newfile == NULL){
-            printf("Error: Could not open file\n");
-            return 1;
+    fclose(file);
+
+    for (int j = 0; j < i; j++) {
+        FILE *newfile = fopen(files[j], "r");
+        if (newfile == NULL) {
+            perror("Error opening log file");
+            continue;  
         }
-        char content[MAX_LINES];
-        while(fgets(content, MAX_LINES, newfile) != NULL){
-            printf("%s", content);
+
+        char content[MAX_LINE_LENGTH];
+        int found = 0;
+        while (fgets(content, sizeof(content), newfile) != NULL) {
+            int match = and_flag ? 1 : 0;
+            for (int a = 2; a < argc; a++) {  
+                if (strstr(content, argv[a]) != NULL) {
+                    if (or_flag) {
+                        match = 1;
+                        break;
+                    }
+                } else {
+                    if (and_flag) {
+                        match = 0;
+                        break;
+                    }
+                }
+            }
+            if (match) {
+                found = 1;
+                break;
+            }
         }
         fclose(newfile);
-    }
-    fclose(file);
-}
 
-
-char *ltrim(char *s) {
-    while (isspace((unsigned char)*s)) {
-        s++;
+        if (found) {
+            printf("Found in %s\n", files[j]);
+        } else {
+            printf("Not found in %s\n", files[j]);
+        }
     }
-    return s;
+
+    return 0;
 }
